6895d03dcb3813b308d50f4c82ee40282445746d
William <lauw26@outlook.com>
2017-08-05 10:51:49 +0100

    Comp random choice completed, function for winner decleared

--- homework/w01d05/rps.js
+++ homework/w01d05/rps.js
@@ -1,11 +1,22 @@
 var playerWon = 0;
 var compWon = 0;
+var comp = ["r","p","s","l","sp"]
+//While loop used for best of 5 so first player to get 3 round wins, wins overall
 alert("Hello!")
 while((playerWon<3)&&(compWon<3))
 {
 	var userChoice = prompt("Please enter the first letter  in lower case of your choice e.g. sp for spock");
-	playerWon += 1;
+	greetings(userChoice);
+	//Maths.random will pick a random number, Maths.floor will round the floating point number.
+	var compChoice = comp[Math.floor(Math.random() * 5)];
+	alert("comp choice is "+ compChoice);
+	playerWon +=3;
 
+}
 
+// Function used to determine winner of round
 
+function greetings(userChoice,compChoice)
+{
+	
 }
