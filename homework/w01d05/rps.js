0f82cd113bd5ebe1b6ee651a13750e2cc866e07f
William <lauw26@outlook.com>
2017-08-05 10:17:32 +0100

    js file linked to html while loop used for best of 5

---
+++ homework/w01d05/rps.js
@@ -0,0 +1,11 @@
+var playerWon = 0;
+var compWon = 0;
+alert("Hello!")
+while((playerWon<3)&&(compWon<3))
+{
+	var userChoice = prompt("Please enter the first letter  in lower case of your choice e.g. sp for spock");
+	playerWon += 1;
+
+
+
+}
