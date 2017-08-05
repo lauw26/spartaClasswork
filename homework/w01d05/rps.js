b7a88e3858f686a7a10da032ba44d278cd2d86a9
William <lauw26@outlook.com>
2017-08-05 11:37:24 +0100

    result created

--- homework/w01d05/rps.js
+++ homework/w01d05/rps.js
@@ -8,10 +8,20 @@ while((playerWon<3)&&(compWon<3))
 	var userChoice = prompt("Please enter the first letter  in lower case of your choice e.g. sp for spock");
 	//Maths.random will pick a random number, Maths.floor will round the floating point number.
 	var compChoice = comp[Math.floor(Math.random() * 5)];
+	var result = decide(userChoice,compChoice);
+	//Using the result to find outcome
+	switch(result){
+		case "win":
 
-	alert(decide(userChoice,compChoice));
+		break;
+		case "lose":
 
-	playerWon +=3;
+		break;
+		case "tie"
+
+		break;
+		default:
+	}
 
 }
 
