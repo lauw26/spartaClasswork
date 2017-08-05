9bbcdb5369b5199eb6e62ea945e5c794e87c2d00
William <lauw26@outlook.com>
2017-08-05 12:09:37 +0100

    Replay implemented

--- homework/w01d05/rps.js
+++ homework/w01d05/rps.js
@@ -1,37 +1,42 @@
 var playerWon = 0;
 var compWon = 0;
 var comp = ["rock","paper","scissor","lizard","spock"]
+var replay;
 //While loop used for best of 5 so first player to get 3 round wins, wins overall
-alert("Hello!")
-while((playerWon<3)&&(compWon<3))
-{
-	var userChoice = prompt("Please enter your choice in lower case e.g. rock");
-	//Maths.random will pick a random number, Maths.floor will round the floating point number.
-	var compChoice = comp[Math.floor(Math.random() * 5)];
-	var result = decide(userChoice,compChoice);
-	//Using the result to find outcome
-	switch(result){
-		case "win":
-			playerWon += 1;
-			alert("You win!\n" + userChoice + " beats " + compChoice + "\nCurrent score player: " + playerWon + " computer: " + compWon);
-		break;
-		case "lose":
-			compWon += 1;
-			alert("You lose!\n" + userChoice + " loses against " + compChoice + "\nCurrent score player: " + playerWon + " computer: " + compWon);
-		break;
-		case "tie":
-			alert("Tie!\n" + userChoice + " ties against " + compChoice + "\nCurrent score player: " + playerWon + " computer: " + compWon);
-		break;
-		default: alert(result)
+alert("Welcome to the best out of 5 rock paper, paper, scissor, lizard, spock game")
+do
+	{
+	while((playerWon<3)&&(compWon<3))
+	{
+		var userChoice = prompt("Please enter your choice in lower case e.g. rock");
+		//Maths.random will pick a random number, Maths.floor will round the floating point number.
+		var compChoice = comp[Math.floor(Math.random() * 5)];
+		var result = decide(userChoice,compChoice);
+		//Using the result to find outcome
+		switch(result){
+			case "win":
+				playerWon += 1;
+				alert("You win!\n" + userChoice + " beats " + compChoice + "\nCurrent score player: " + playerWon + " computer: " + compWon);
+			break;
+			case "lose":
+				compWon += 1;
+				alert("You lose!\n" + userChoice + " loses against " + compChoice + "\nCurrent score player: " + playerWon + " computer: " + compWon);
+			break;
+			case "tie":
+				alert("Tie!\n" + userChoice + " ties against " + compChoice + "\nCurrent score player: " + playerWon + " computer: " + compWon);
+			break;
+			default: alert(result);
+		}
 	}
-}
-//Determine the overall winner of the 5 matches
-if(playerWon>compWon){
-	alert("Congratulations you win!\n Final score Player "+ playerWon + " computer: " + compWon)
-}else{
-	alert("Better luck next time You lost!!\n Final score Player "+ playerWon + " computer: " + compWon)
-}
+	//Determine the overall winner of the 5 matches
+	if(playerWon>compWon){
+		alert("Congratulations you win!\n Final score Player "+ playerWon + " computer: " + compWon);
+	}else{
+		alert("Better luck next time You lost!\n Final score Player "+ playerWon + " computer: " + compWon);
+	}
 
+	replay = prompt("Play again? y/n");
+}while(replay == "y")
 // Function used to determine winner of round
 
 function decide(userChoice,compChoice)
