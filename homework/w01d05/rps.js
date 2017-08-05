baee60d5a5339af39128aae1cb227a1d961ea97a
William <lauw26@outlook.com>
2017-08-05 11:02:28 +0100

    Function renamed, implemented case for rock

--- homework/w01d05/rps.js
+++ homework/w01d05/rps.js
@@ -6,17 +6,49 @@ alert("Hello!")
 while((playerWon<3)&&(compWon<3))
 {
 	var userChoice = prompt("Please enter the first letter  in lower case of your choice e.g. sp for spock");
-	greetings(userChoice);
 	//Maths.random will pick a random number, Maths.floor will round the floating point number.
 	var compChoice = comp[Math.floor(Math.random() * 5)];
-	alert("comp choice is "+ compChoice);
+
+	alert(decide(userChoice,compChoice));
+
 	playerWon +=3;
 
 }
 
 // Function used to determine winner of round
 
-function greetings(userChoice,compChoice)
+function decide(userChoice,compChoice)
 {
-	
+	switch(userChoice){
+
+		case "r":
+				switch(compChoice){
+
+					case "r":
+						return "tie"
+					break;
+
+					case "p":
+						return "lose"
+					break;
+
+					case "s":
+						return "win"
+					break;
+
+					case "l":
+						return "win"
+					break;
+
+					case "sp":
+						return "lose"
+					break;
+
+
+				}
+		break;
+
+
+
+	}
 }
