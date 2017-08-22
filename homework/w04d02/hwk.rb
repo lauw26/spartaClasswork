e60f1ac44e2f0a6b480eb4dd1bd2ca96f54882c2
William <lauw26@outlook.com>
2017-08-22 17:35:46 +0100

    added songs to url

--- homework/w04d02/hwk.rb
+++ homework/w04d02/hwk.rb
@@ -2,41 +2,41 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 # new using get to acquire form
-get "/new" do
+get "/songs/new" do
 	"Make new form"	
 end
 
 # Index to show using get
-get "/" do
+get "/songs" do
 	"index"	
 end
 
 # get a certain thing
-get "/:id" do
+get "/songs/:id" do
 	"display id thing"	
 end
 
 # use post to create new thing
-post "/" do
+post "/songs" do
 	"creating new thing"	
 end
 
 # edit a certain thing using id
-get "/:id/edit" do
+get "/songs/:id/edit" do
 	"edit certain thing"	
 end
 
 # update certain thing using put
-put "/:id" do
+put "/songs/:id" do
 	"update certain thing with put"	
 end
 
 # update certain thing using patch
-patch "/:id" do
+patch "/songs/:id" do
 	"update certain thing with patch"	
 end
 
 # delete certain thing using delete
-delete "/:id" do
+delete "/songs/:id" do
 	"delete certain thing"	
 end
