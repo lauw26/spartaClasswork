e73e69223f8b3041bb6d26777783aedf2b9daac1
William <lauw26@outlook.com>
2017-08-22 17:21:31 +0100

    show route

--- homework/w04d02/hwk.rb
+++ homework/w04d02/hwk.rb
@@ -1,8 +1,12 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
-# new using get
-
+# new using get to acquire form
 get "/new" do
 	"Make new form"	
+end
+
+# Index to show using get
+get "/" do
+	"Show display"	
 end
