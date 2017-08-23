a6356de53fdceeebee2552c2fedac549e4f677e6
William <lauw26@outlook.com>
2017-08-23 19:54:45 +0100

    nav bar implemented

--- homework/w04d03/controllers/songs_controller.rb
+++ homework/w04d03/controllers/songs_controller.rb
@@ -1,22 +1,6 @@
 class SongsController < Sinatra::Base
 	# array of objects to be posted on website
-	# $posts = [
-	# 	{
-	# 	id: 0,
-	# 	title: "Post 1",
-	# 	body: "This is is the first post"
-	# 	},
-	# 	{
-	# 	id: 1,
-	# 	title: "Post 2",
-	# 	body: "This is is the second post"
-	# 	},
-	# 	{
-	# 	id: 2,
-	# 	title: "Post 3",
-	# 	body: "This is is the third post"
-	# 	}
-	# ]
+
 	# sets root as the parent-directory of the current file
 	# goes back to root directory using ..
 	set :root, File.join(File.dirname(__FILE__), "..")
@@ -30,31 +14,32 @@ class SongsController < Sinatra::Base
 	end
 
 	get "/" do
+		@title = "Ollie is sick"
 		erb :"host/index"
 	end
 	
-	get "/new" do
+	# get "/new" do
 		
-	end
+	# end
 
-	get "/:id" do
+	# get "/:id" do
 
-	end
+	# end
 
-	post "/" do
+	# post "/" do
 
-	end
+	# end
 
-	put "/:id" do
+	# put "/:id" do
 		
-	end
+	# end
 
-	delete "/:id" do
+	# delete "/:id" do
 
-	end
+	# end
 
-	get "/:id/edit" do
+	# get "/:id/edit" do
 
-	end
+	# end
 
 end
