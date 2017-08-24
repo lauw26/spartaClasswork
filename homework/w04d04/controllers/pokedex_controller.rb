057c546ac37ac04ee2493d685a264de9bbcdf332
William <lauw26@outlook.com>
2017-08-24 20:33:05 +0100

    form created along and linked to new url

--- homework/w04d04/controllers/pokedex_controller.rb
+++ homework/w04d04/controllers/pokedex_controller.rb
@@ -1,6 +1,13 @@
 require_relative "pokemon.rb"
 class PokedexController < Sinatra::Base
 	$pokemons = []
+	poke1des = "A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon."
+	pokemon1 = Pokemon.new(1,"Bulbasaur","Grass/Posion","Starter Pokémon from Professor Oak in Pallet Town",poke1des)
+	poke2des = "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs"
+	pokemon2 = Pokemon.new(2,"Ivysaur","Grass/Posion","Evolve Bulbasaur",poke2des)
+	poke3des = "The plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight."
+	pokemon3 = Pokemon.new(3,"Venusaur","Grass/Posion","Evolve Ivysaur",poke3des)
+	$pokemons.push(pokemon1,pokemon2,pokemon3)
 	# sets root as the parent-directory of the current file
 	# goes back to root directory using ..
 	set :root, File.join(File.dirname(__FILE__), "..")
@@ -18,11 +25,13 @@ class PokedexController < Sinatra::Base
 		"INDEX"
 		# to be used for index 
 		@title = "Pokedex"
+		@pokemons = $pokemons
 		erb :"host/index"
 	end
 	
 	get "/new" do
-		
+		@pokemon = Pokemon.new("","","","","")
+		erb :"host/new"
 	end
 
 	get "/:id" do
--- homework/w04d04/controllers/pokemon.rb
+++ homework/w04d04/controllers/pokemon.rb
@@ -1,11 +1,12 @@
 # blueprint for pokemon objects
 class Pokemon
-	attr_accessor :id, :name, :type, :description
+	attr_accessor :id, :name, :type, :location, :description
 
-	def initialize id, name,type,description
+	def initialize id, name,type,location,description
 		@id = id
 		@name = name
 		@type = type
+		@location = location
 		@description = description
 	end
 end
