c899eabc25bd2c1e2d5fd7548293eadd3848dffb
William <lauw26@outlook.com>
2017-08-25 06:50:54 +0100

    sorts pokemon by index before display annd eiditable

--- homework/w04d04/controllers/pokedex_controller.rb
+++ homework/w04d04/controllers/pokedex_controller.rb
@@ -2,11 +2,11 @@ require_relative "pokemon.rb"
 class PokedexController < Sinatra::Base
 	$pokemons = []
 	poke1des = "A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon."
-	pokemon1 = Pokemon.new("1","Bulbasaur","Grass/Posion","Starter Pokémon from Professor Oak in Pallet Town",poke1des)
+	pokemon1 = Pokemon.new(0,"1","Bulbasaur","Grass/Posion","Starter Pokémon from Professor Oak in Pallet Town",poke1des)
 	poke2des = "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs"
-	pokemon2 = Pokemon.new("2","Ivysaur","Grass/Posion","Evolve Bulbasaur",poke2des)
+	pokemon2 = Pokemon.new(1,"2","Ivysaur","Grass/Posion","Evolve Bulbasaur",poke2des)
 	poke3des = "The plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight."
-	pokemon3 = Pokemon.new("3","Venusaur","Grass/Posion","Evolve Ivysaur",poke3des)
+	pokemon3 = Pokemon.new(2,"3","Venusaur","Grass/Posion","Evolve Ivysaur",poke3des)
 	$pokemons.push(pokemon1,pokemon2,pokemon3)
 	# sets root as the parent-directory of the current file
 	# goes back to root directory using ..
@@ -23,39 +23,42 @@ class PokedexController < Sinatra::Base
 
 	get "/" do
 		"INDEX"
+
 		# to be used for index 
 		@title = "Pokedex"
-		@pokemons = $pokemons
+		@pokemons = $pokemons.sort_by {|pokemon| pokemon.index.to_i}
 		erb :"host/index"
 	end
 	#Brings form for new entry into pokedex
 	get "/new" do
-		@pokemon = Pokemon.new("","","","","")
+		@pokemon = Pokemon.new("","","","","","")
 		erb :"host/new"
 	end
 	#Shows pokemon display
 	get "/:id" do
 		@id = params[:id]
-		@pokemon = $pokemons[@id.to_i-1]
+		@pokemon = $pokemons[@id.to_i]
 		erb :"host/show"
 	end
 	#Create pokemon 
 	post "/" do
-		id = params[:id]
+		position = $pokemons.length
+		id = $pokemons.length
+		index = params[:index]
 		name = params[:name]
 		type = params[:type]
 		location = params[:location]
 		description = params[:description]
-		pokemon = Pokemon.new(id,name,type,location,description)
+		pokemon = Pokemon.new(id,index,name,type,location,description)
 		$pokemons.push(pokemon)
 		redirect "/"
 		
 	end
 	#Update existing pokemon
 	put "/:id" do
-		id = params[:id].to_i - 1
+		id = params[:id].to_i
 		pokemon = $pokemons[id]
-		pokemon.id = params[:id]
+		pokemon.index = params[:index]
 		pokemon.name = params[:name]
 		pokemon.type = params[:type]
 		pokemon.location = params[:location]
@@ -73,7 +76,7 @@ class PokedexController < Sinatra::Base
 	#edit pokemon
 	get "/:id/edit" do
 		@id = params[:id]
-		@pokemon = $pokemons[@id.to_i-1]
+		@pokemon = $pokemons[@id.to_i]
 		erb :"host/edit"
 	end
 end
--- homework/w04d04/controllers/pokemon.rb
+++ homework/w04d04/controllers/pokemon.rb
@@ -1,9 +1,10 @@
 # blueprint for pokemon objects
 class Pokemon
-	attr_accessor :id, :name, :type, :location, :description
+	attr_accessor :id, :index, :name, :type, :location, :description
 
-	def initialize id, name,type,location,description
+	def initialize id, index, name,type,location,description
 		@id = id
+		@index = index
 		@name = name
 		@type = type
 		@location = location
