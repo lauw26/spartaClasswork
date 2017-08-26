d4193de3527ba628c843594914fbf4ffc5a93c0b
William <lauw26@outlook.com>
2017-08-26 17:19:08 +0100

    show retrieves back one object

--- homework/w04d05/models/champion.rb
+++ homework/w04d05/models/champion.rb
@@ -35,4 +35,26 @@ class Champion
 
 		champions
 	end
+	#Selecting only 1 item and returning it then seeding as a object
+	def self.find id
+		conn = self.open_connection
+		sql = "SELECT id, name, position, cost, passive, ability1, ability2, ability3, ultimate, lore FROM champion WHERE id = #{id} LIMIT 1"
+		champions = conn.exec(sql)
+		champion = self.hydrate champions[0]
+
+		champion
+
+	end
+	# Saves new entry and inputs to database since it static and a instance has been created have to use (instance name).connect rather than self.connect
+	def save 
+		conn = Post.open_connection
+		#if there is no id then make new entry
+		if(!self.id)
+			sql = "INSERT INTO champion (title, body) VALUES ('#{self.title}','#{self.body}')"
+		else
+			# updates using id if id exist
+			sql = "UPDATE post SET title='#{self.title}', body='#{self.body}' WHERE id=#{self.id}"
+		end
+		conn.exec(sql)
+	end
 end
