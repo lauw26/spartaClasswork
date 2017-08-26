3575afdf2e4ed09f67a3138100c85a6ff5248a3b
William <lauw26@outlook.com>
2017-08-26 17:09:12 +0100

    index function to acquire data from db implemented

--- homework/w04d05/models/champion.rb
+++ homework/w04d05/models/champion.rb
@@ -6,6 +6,7 @@ class Champion
 	end
 	#make data retrieved into an object
 	def self.hydrate data
+		# make new champ object and using db data to seed it
 		champion = Champion.new
 		champion.id = data['id']
 		champion.name = data['name']
@@ -15,7 +16,23 @@ class Champion
 		champion.ability1 = data['ability1']
 		champion.ability2 = data['ability2']
 		champion.ability3 = data['ability3']
+		champion.ultimate = data['ultimate']
+		champion.lore = data['lore']
+		# return object seeded from db
+		champion
+	end
 
-		post
+	#Index method to connect to db
+	def self.all
+		conn = self.open_connection
+		# grab everything in db and order them by name
+		sql = "SELECT id, name, position, cost, passive, ability1, ability2, ability3, ultimate, lore FROM champion ORDER BY name" 
+		results = conn.exec(sql)
+
+		champions = results.map do |result|
+			self.hydrate result
+		end
+
+		champions
 	end
 end
