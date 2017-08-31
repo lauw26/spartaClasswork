d8752524ae0b7fdc70c4a106de54460a7b732d28
Tech-A03 <tech-a03@tech-a03s-air-2.home>
2017-08-31 21:29:56 +0100

    Display user show working with user messages

--- classwork/w05d04/auth/devise2/app/controllers/users_controller.rb
+++ classwork/w05d04/auth/devise2/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
 	def index
 	end
+	def show
+		@user = User.find current_user.id
+		@posts = @user.posts
+	end
 end
--- classwork/w05d04/auth/devise2/config/routes.rb
+++ classwork/w05d04/auth/devise2/config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   root 'static#home'
+  get '/user/:id' => 'users#show'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
